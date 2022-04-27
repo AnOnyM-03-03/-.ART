2d19311c210779df16daa999e4642b19f0f5939d
Dmitrij Tolstov <tolstov.dmitrij42@gmail.com>
2022-04-27 21:24:10 +0400

    add import "slider"

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,12 @@
+import { slider } from './modules';
+
 window.addEventListener('DOMContentLoaded', () => {
-    'use strict'
+   'use strict';
+   slider(
+      '.feedback-slider-item',
+      'horizontal',
+      '.main-prev-btn',
+      '.main-next-btn'
+   );
+   slider('.main-slider-item', 'vertical');
 });
