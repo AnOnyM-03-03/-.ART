b8121575f3e1b2f8194fe7526c295e7580d4c802
Dmitrij Tolstov <tolstov.dmitrij42@gmail.com>
2022-05-01 13:04:22 +0400

    add import and export

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,11 @@
-import { modals } from './modules';
+import { modals, showMoreStyles } from './modules';
 
 window.addEventListener('DOMContentLoaded', () => {
    'use strict';
    modals();
+   const btnShowMoreStyles = {
+      trigger: '.button-styles',
+      styles: '.styles-2',
+   };
+   showMoreStyles(btnShowMoreStyles);
 });
