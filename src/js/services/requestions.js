d564f19d11e18fc98867d66e16d8beec87a4b90e
Dmitrij Tolstov <tolstov.dmitrij42@gmail.com>
2022-05-03 12:27:25 +0400

    creating a request from the server

---
+++ src/js/services/requestions.js
@@ -0,0 +1,18 @@
+export const postData = async (url, data) => {
+   const res = await fetch(url, {
+      method: 'POST',
+      body: data,
+   });
+
+   return await res.text();
+};
+
+export const getResource = async (url) => {
+   const res = await fetch(url);
+
+   if (!res.ok) {
+      throw new Error(`Could not fetch ${url}, status: ${res.status}`);
+   }
+
+   return await res.json();
+};
