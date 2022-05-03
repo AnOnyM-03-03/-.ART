cafe938de90db38e2f583d88b39180fd1faa4e24
Dmitrij Tolstov <tolstov.dmitrij42@gmail.com>
2022-05-03 20:46:17 +0400

    adding blocks try and catch

--- src/js/services/requestions.js
+++ src/js/services/requestions.js
@@ -1,18 +1,26 @@
 export const postData = async (url, data) => {
-   const res = await fetch(url, {
-      method: 'POST',
-      body: data,
-   });
+   try {
+      const res = await fetch(url, {
+         method: 'POST',
+         body: data,
+      });
 
-   return await res.text();
-};
-
-export const getResource = async (url) => {
-   const res = await fetch(url);
-
-   if (!res.ok) {
-      throw new Error(`Could not fetch ${url}, status: ${res.status}`);
+      return await res.text();
+   } catch (error) {
+      console.log('Error');
    }
-
-   return await res.json();
 };
+
+   export const getResource = async (url) => {
+      try {
+         const res = await fetch(url);
+
+         if (!res.ok) {
+            throw new Error(`Could not fetch ${url}, status: ${res.status}`);
+         }
+
+         return await res.json();
+      } catch (error) {
+         console.log('Error');
+      }
+   };
