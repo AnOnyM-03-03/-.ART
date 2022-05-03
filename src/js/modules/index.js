83570af61299ba6447a34b9a6ba5a827b521fee4
Dmitrij Tolstov <tolstov.dmitrij42@gmail.com>
2022-05-03 21:09:04 +0400

    add exports

--- src/js/modules/index.js
+++ src/js/modules/index.js
@@ -2,4 +2,7 @@ export { slider } from './slider';
 export { modals } from './modals';
 export { mask } from './mask';
 export { checkInputs } from './checkinputs';
-
+export { calc } from './calc';
+export { showMoreStyles } from './showMoreStyles';
+export { form } from './forms';
+export { filter } from './filtres';
