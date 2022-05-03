74c4634946af0259430d713a52b1496ae8103a7a
Dmitrij Tolstov <tolstov.dmitrij42@gmail.com>
2022-05-03 12:28:42 +0400

    creating dynamic blocks

--- src/js/modules/showMoreStyles.js
+++ src/js/modules/showMoreStyles.js
@@ -1,26 +1,34 @@
-export const showMoreStyles = ({ trigger, styles }) => {
-   const cards = document.querySelectorAll(styles),
-      btn = document.querySelector(trigger);
+import { getResource } from '../services/requestions';
 
-   cards.forEach((card) => card.classList.add('animated', 'fadeInUp'));
+export const showMoreStyles = ({ trigger, wrapper }) => {
+   const btn = document.querySelector(trigger);
 
-   btn.addEventListener('click', () => {
-      cards.forEach((card) => {
-         card.classList.remove(
-            'hidden-lg',
-            'hidden-md',
-            'hidden-sm',
-            'hidden-xs'
-         );
-         card.classList.add(
+   btn.addEventListener('click', function () {
+      getResource('http://localhost:3000/styles')
+         .then((res) => createCards(res))
+         .catch((error) => console.log(error));
+      this.remove();
+   });
+
+   function createCards(responce) {
+      responce.forEach(({ src, link, title }) => {
+         let cards = document.createElement('div');
+         cards.classList.add(
+            'animated',
+            'fadeInUp',
             'col-sm-3',
             'col-sm-offset-0',
             'col-xs-10',
             'col-xs-offset-1'
          );
+         cards.innerHTML = `
+            <div class="styles-block">
+                <img src=${src} alt="style">
+                <h4>${title}</h4>
+                <a href=${link}>Подробнее</a>
+            </div>
+            `;
+         document.querySelector(wrapper).appendChild(cards);
       });
-
-      btn.remove();
-      btn.style.display = 'none';
-   });
+   }
 };
