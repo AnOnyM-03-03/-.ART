a9c4a95df6095e2a1107e872d8cc6bd989672e8a
Dmitrij Tolstov <tolstov.dmitrij42@gmail.com>
2022-05-03 14:26:12 +0400

    export calc function

--- src/js/modules/index.js
+++ src/js/modules/index.js
@@ -2,3 +2,4 @@ export { slider } from './slider';
 export { modals } from './modals';
 export { showMoreStyles } from './showMoreStyles';
 export { form } from './forms';
+export { calc } from './calc';
