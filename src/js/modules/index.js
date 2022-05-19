e1ba74e17338f96331b944d0a0a4f7fb68086a5f
Dmitrij Tolstov <tolstov.dmitrij42@gmail.com>
2022-05-19 14:29:47 +0400

    rename path 'checkInputs'

--- src/js/modules/index.js
+++ src/js/modules/index.js
@@ -1,7 +1,7 @@
 export { slider } from './slider';
 export { modals } from './modals';
 export { mask } from './mask';
-export { checkInputs } from './checkinputs';
+export { checkInputs } from './checkInputs';
 export { calc } from './calc';
 export { showMoreStyles } from './showMoreStyles';
 export { form } from './forms';
